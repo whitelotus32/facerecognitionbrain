405ca0f557bc8fbba03f2c049cb542b0c165251e
Nicholas Dixon <NickDixon32@proton.me>
2024-06-07 02:34:52 -0500

    Added a navigation component and worked on dependency errors.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-
+// import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        {/* {<Logo />
+        <ImageLinkForm />
+        <FaceRecognition />} */}
+      </div>
+    );
+  }
 }
 
 export default App;
