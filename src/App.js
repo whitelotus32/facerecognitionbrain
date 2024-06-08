49eae091860348af0955f310c1d6adde2fd37517
Nicholas Dixon <NickDixon32@proton.me>
2024-06-08 04:04:29 -0500

    Created a state to add more functionality.

--- src/App.js
+++ src/App.js
@@ -21,6 +21,21 @@ const particlesOptions = {
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,7 +45,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit} 
+        />
         {/*
         <FaceRecognition />} */}
       </div>
