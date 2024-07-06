86509a7b92d2f7ff5454b371abc44b432727e1e8
Nicholas Dixon <naseer2152@gmail.com>
2024-07-05 19:04:49 -0500

    Created a new endpoint to hide the API key.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Rank from './components/Rank/Rank';
 import Navigation from './components/Navigation/Navigation';
 import SignIn from './components/SignIn/SignIn';
@@ -10,10 +9,6 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-
-const app = new Clarifai.App({
-  apikey: 'INSERT_API_HERE'
-});
 
 const particlesOptions = {
   particles: {
@@ -27,23 +22,25 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageURL: '',
+  box: {},
+  route: 'SignIn',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageURL: '',
-      box: {},
-      route: 'SignIn',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
+    this.state = initialState;
     }
   }
 
@@ -78,12 +75,15 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  onPictureSubmit = () => {
+  onButtonSubmit = () => {
     this.setState=({imageURL: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+          input: this.state.input
+      })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
@@ -97,23 +97,23 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count } ))
             })
+            .catch(console.log)
 
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
-        .catch(err => console.log(err));
-
+        .catch(err => console.log(err))
   }
 
   onRouteChange = (route) => {
     if(route === 'SignOut') {
-      this.setState({isSignedIn: true})
+      this.setState(initialState)
     } else if(route === 'home') {
       this.setState({isSignedIn: true})
     }
   }
 
-  render() {
+  render(); {
     const { isSignedIn, imageURL, route,  box } = this.state;
     return (
       <div className="App">
@@ -140,6 +140,5 @@ class App extends Component {
       </div>
     );
   }
-}
 
 export default App;
